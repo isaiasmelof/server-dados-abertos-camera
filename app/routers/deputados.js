7cc34fc730d9ebb43c1e02fee28fd3d63ada5501
Isaias Fernandes <ifm394@gmail.com>
2017-07-29 13:22:09 -0300

    Adicionando rota que recupera deputados baseado em 3 tipos de filtros enviados no parametro da requisao. (uf, nome epartido)

--- app/routers/deputados.js
+++ app/routers/deputados.js
@@ -2,16 +2,63 @@ var express = require('express')
 var request = require('request')
 var parser = require('xml2json');
 var app = express()
+
+//Rota que recupera todos os deputados
 app.get('/allDeputados',(req, res, err) => {
    request({uri: 'http://www.camara.leg.br/SitCamaraWS/Deputados.asmx/ObterDeputados',
-            method: 'GET',
-            timeout: 10000,
-            followRedirect: true,
-            maxRedirects: 10}, function (error, response, body){
+            method: 'GET'}, function (error, response, body){
         res.set('Content-Type', 'application/json');
-        var xml = body
-        var json = parser.toJson(xml);
-        res.send(json)
+        res.send(parser.toJson(body))
    })
 })
-app.listen(3000)
+/* Rota que recupera deputado(s) que atedem a um determinado filtro enviado no parametro da requisicao. Os filtros aceitos são:
+    
+    uf: representa a unidade federativa do deputado.
+    partido: sigla a que representa o partido no qual o deputado é filiado.
+    nome: parte do nome que está contida no nome do deputado desejado. (contains)
+
+    Os parametros podem ser passados conforme é desejado, e são avaliados em um 'and'.
+   
+    Ex: Caso seja enviado os parametros uf e partido, o deputado só será retornado como resultado caso o partido e a uf estejam igual ao filtro. 
+    Caso seja enviado apenas a uf, somente este criterio será avaliado.
+*/
+app.get('/deputados',(req, res, err)=> {
+    request({uri: 'http://www.camara.leg.br/SitCamaraWS/Deputados.asmx/ObterDeputados',
+            method: 'GET'}, function (error, response, body){
+        res.set('Content-Type', 'application/json');
+        var parametros = req.query
+
+        var objJson = JSON.parse(parser.toJson(body))
+
+        var newArray = objJson.deputados.deputado.filter((deputado) => {
+            return filtrarDeputado(deputado, parametros)
+        })
+        res.status(200).json(newArray)
+    })    
+})
+
+/**
+ * Avalia se um deputado atende aos parametros de filtro enviado.
+ * @param {*} deputado - Representa um deputado no qual será avaliado se este atende os filtros desejados.
+ * @param {*} parametros - Parametros com criterios de filtro que serão testados. (uf, partido ou nome)
+ * @return true - caso o deputado atenda aos filtros. 
+ *         false - caso não atenda.
+ */
+function filtrarDeputado(deputado /* Objeto do Deputado */, parametros /* Query */) {
+    var retorno = true
+
+    if(parametros.partido) {
+        console.log(deputado.partido)
+        retorno = retorno && (parametros.partido == deputado.partido)
+    }
+
+    if(parametros.nome){
+         retorno = retorno && (deputado.nome.includes(parametros.nome))
+    }
+    
+    if(parametros.uf){
+        retorno = retorno && (deputado.uf == parametros.uf)
+    }
+    return retorno
+}
+app.listen(3000);
