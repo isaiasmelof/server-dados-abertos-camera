df87474c58739e795b820fee45bf179a79b22151
Isaias Fernandes <ifm394@gmail.com>
2017-07-31 16:37:12 -0300

    Adicionando rota para raiz do projeto

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ var deputadosRoutes = require('./app/routers/deputados.js')
 var app = express()
 
 app.use(deputadosRoutes)
-
+app.get('/',(req,res,err)=>{
+    res.send('Server Up!')
+})
 app.listen(3000)
