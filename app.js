1d1a096ec695e6dda23f29ec4177c42afaf9401a
Isaias Melo <ifm394@gmail.com>
2017-08-11 16:29:01 -0300

    Editando configurações de porta do server

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ var app = express()
 app.use(deputadosRoutes)
 app.use(proposicoesRoutes)
 
+app.set('port', (process.env.PORT || 5000))
 app.get('/home ',(req,res,err)=>{
     res.send({'msg':' bem vindo ao meu server'})
 })
-app.listen(3000)
+
+
+
+app.listen(app.get('port'), function(){
+    console.log('App is running server is listening on port',app.get('port'))
+})
